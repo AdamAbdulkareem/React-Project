191c6523be27426ea3890c68682c1dcf68fa3b25
AdamAbdulkareem <dmabdulkareem@gmail.com>
2024-08-19 22:09:47 +0100

    Deleted some files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const header = (
+  <header className="header">
+    <h1>Front End Technologies</h1>
+    <div className="header-content">
+    <li><img src="/exercise-01/css-3.svg" alt="" /></li>
+    <li><img src="/exercise-01/html-1.svg" alt="" /></li>
+    <li><img src="/exercise-01/javascript-1.svg" alt="" /></li>
+    <li><img src="/exercise-01/react-2.svg" alt="" /></li>
+    </div>
+  </header>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root")
+ReactDOM.render(header, rootElement)
